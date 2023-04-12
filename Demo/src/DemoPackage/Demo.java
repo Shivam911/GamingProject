bfb8def7fe837ad3580ce9cfcdaf181ff83f03fe
Shivam <shivambhadauria111@gmail.com>
2023-04-12 14:56:21 +0530

    Moving a demo project to a remote repository

---
+++ Demo/src/DemoPackage/Demo.java
@@ -0,0 +1,13 @@
+package DemoPackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		System.out.println("I am a demo project");
+
+	}
+
+}
